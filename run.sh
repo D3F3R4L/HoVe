86c91d02c8d3b9090e4a776e47b91c07ec6c4d6e
lucassid <lucassidpacheco@gmail.com>
2019-06-03 22:38:38 -0300

    backup logs as well

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+
+
+#--------------------------------------------
 echo -n "how many times do you wish to run? "
 read n
-
 echo -n "whats the algorithm used? "
 read alg
-
 echo -n "Video to be used: "
 read video
 
+#-------------------------------------------
 if [ "$video" == "container" ]
 then
   video_id=2
@@ -39,13 +42,16 @@ else
     exit 1
 fi
 
+#-------------------------------------------------------
 sed -i "s/#define video [0-9]/#define video $video_id/g" scratch/v2x_3gpp_small.cc
 
+
+#------------------------------------------------------
 ./waf configure
 mkdir ${alg}
 for i in $(seq 1 $n)
 do
     time ./waf --run "scratch/v2x_3gpp_small --handoverAlg=$alg --seedValue=$i" > log_${alg}_${i} 2>&1
     mkdir ${alg}/simul${i}
-    mv v2x_temp/* $alg/simul$i
+    mv v2x_temp/* log_${alg}_${i} $alg/simul$i
 done
