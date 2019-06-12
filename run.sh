c851976d37b40075c7ed92cbf1fe66d77bf90f53
lucassid <lucassidpracheco@gmail.com>
2019-06-12 16:18:12 -0300

    command line parameters implemented in run script

--- run.sh
+++ run.sh
@@ -1,45 +1,80 @@
 #!/bin/bash
 
-if [ $# -eq 3 ]
+#default values
+n=1
+alg="hove"
+video="highway"
+ue_n=60
+enb_n=100
+
+# check if arguments were passed
+if [ $# -gt 0 ]
 then
-	n=$1
-	alg=$2
-	video=$video
+  # read command line arguments
+  for i in "$@"
+  do
+    case $i in
+        -n=*|--simul-number=*)
+        n="${i#*=}"
+        shift
+        ;;
+        -alg=*|--algorithm=*)
+        alg="${i#*=}"
+        shift
+        ;;
+        -v=*|--video=*)
+        video="${i#*=}"
+        shift
+        ;;
+        -uen=*|--ue-number=*)
+        ue_n="${i#*=}"
+        shift
+        ;;
+        -enbn=*|--enb-number=*)
+        enb_n="${i#*=}"
+        shift
+        ;;
+        *)
+        ;;
+    esac
+  done
 
 else
+  # if no arguments, read one by one
 	echo -n "how many times do you wish to run? "
 	read n
 	echo -n "whats the algorithm used? "
 	read alg
 	echo -n "Video to be used: "
 	read video
-
 fi
 
+# translate video to id
 case $video in 
-"container")
-  video_id=2;;
 "highway")
   video_id=1;;
+"container")
+  video_id=2;;
 "highway600")
   video_id=3;;
 "akiyo")
   video_id=4;;
 "masha")
   video_id=7;;
-"despacito")
-  video_id=9;;
 "babyshark")
   video_id=8;;
+"despacito")
+  video_id=9;;
 *)
     exit 1
 esac
 
-echo $video_id
-exit 1
+# change id in file
+sed -i "s/\(#define video \)[0-9]/\1$video_id/g" scratch/v2x_3gpp_small.cc
+sed -i "s/\(node_ue = \)[0-9]\{,3\}/\1$ue_n/g" scratch/v2x_3gpp_small.cc
+sed -i "s/\(low_power = \)[0-9]\{,3\}/\1$enb_n/g" scratch/v2x_3gpp_small.cc
 
-sed -i "s/#define video [0-9]/#define video $video_id/g" scratch/v2x_3gpp_small.cc
-
+# run simulation
 ./waf configure
 mkdir ${alg}
 for i in $(seq 1 $n)
